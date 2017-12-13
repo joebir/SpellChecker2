741efa2aa81b8f985b1b60ec4638093fe3f2d663
Joseph Bir <joe.bir.code@gmail.com>
2017-12-13 14:26:07 -0500

    Implemented SpellController

--- SpellChecker.Web/Controllers/SpellController.cs
+++ SpellChecker.Web/Controllers/SpellController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNet.Identity;
-using SpellChecker.Data;
-using SpellChecker.Models;
+﻿using SpellChecker.Contracts;
 using SpellChecker.Services;
 using System;
 using System.Collections.Generic;
@@ -12,51 +10,24 @@ namespace SpellChecker.Web.Controllers
 {
     public class SpellController : Controller
     {
-        private SpellService GetSpellService()
+        private readonly Lazy<ISpell> _spellService;
+
+        public SpellController()
         {
-            var svc = new SpellService();
-            return svc;
+            _spellService = new Lazy<ISpell>(() =>
+                new SpellService());
         }
 
-        [Authorize]
-        private EntryService GetEntryService()
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var user =
-                    ctx.Users
-                    .Single(e => e.Id == User.Identity.GetUserId());
-
-                return new EntryService(user.CurrentSpellbook);
-            }
-        }
-
-        // GET: Spell
         public ActionResult Index()
         {
-            var model = GetSpellService().GetAllSpells();
+            var model = _spellService.Value.GetAllSpells();
             return View(model);
         }
 
         public ActionResult Details(int id)
         {
-            var model = GetSpellService().GetSpellById(id);
+            var model = _spellService.Value.GetSpellById(id);
             return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(int id)
-        {
-            if (!GetEntryService().CreateEntry(id))
-            {
-                TempData["SaveResult"] = "Unable to add the spell to your spellbook.";
-                return RedirectToAction("Index", "SpellController");
-            }
-
-            TempData["SaveResult"] = "The spell was added to your spellbook.";
-
-            return RedirectToAction("Index");
         }
     }
 }
